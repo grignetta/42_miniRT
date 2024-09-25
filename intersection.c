6b71d6a150c0d0681659a4b7c318d059d1ef6655
daria <d.padenkova@gmail.com>
2024-09-25 17:41:22 +0200

    fix: cylinder center coordinates moved from center of bottom cap to the center of the axis

--- intersection.c
+++ intersection.c
@@ -56,31 +56,55 @@ int intersect_ray_plane(ray_params params, plane *pl, double *t)
 
 int intersect_ray_cylinder(ray_params params, cylinder *cyl, intersection_result *result)
 {
+    // Compute cap centers
+    vector half_height_vector = vector_scale(cyl->axis, cyl->height / 2);
+    vector bottom_center = vector_sub(cyl->center, half_height_vector);
+    vector top_center = vector_add(cyl->center, half_height_vector);
+
 	double t1;
 	double t2;
 	intersection point;
     // Translate the ray and cylinder to a common origin, typically the cylinder's center
     point.CO = vector_sub(params.O, cyl->center);
 
+    // Compute the components perpendicular to the axis
+    vector D_perp = vector_sub(params.D, vector_scale(cyl->axis, vector_dot(params.D,     cyl->axis)));
+    vector CO_perp = vector_sub(point.CO, vector_scale(cyl->axis, vector_dot(point.CO,  cyl->axis)));
+
     // Coefficients for the quadratic equation
-    point.a = params.D.x * params.D.x + params.D.z * params.D.z;
-    point.b = 2 * (point.CO.x * params.D.x + point.CO.z * params.D.z);
-    point.c = point.CO.x * point.CO.x + point.CO.z * point.CO.z - cyl->radius * cyl->radius;
+    // point.a = params.D.x * params.D.x + params.D.z * params.D.z;
+    // point.b = 2 * (point.CO.x * params.D.x + point.CO.z * params.D.z);
+    // point.c = point.CO.x * point.CO.x + point.CO.z * point.CO.z - cyl->radius * cyl->radius;
+
+    point.a = vector_dot(D_perp, D_perp);
+    point.b = 2 * vector_dot(D_perp, CO_perp);
+    point.c = vector_dot(CO_perp, CO_perp) - cyl->radius * cyl->radius;
 
     // Discriminant
     point.discriminant = point.b * point.b - 4 * point.a * point.c;
     if (point.discriminant < 0)
         return 0; // No intersection
-    t1 = (-point.b + sqrt(point.discriminant)) / (2 * point.a);
-    t2 = (-point.b - sqrt(point.discriminant)) / (2 * point.a);
+    t2 = (-point.b + sqrt(point.discriminant)) / (2 * point.a);//t1
+    t1 = (-point.b - sqrt(point.discriminant)) / (2 * point.a);//t2
 
     // Check if the intersection points are within the cylinder's height
-    double y1 = params.O.y + t1 * params.D.y;
-    double y2 = params.O.y + t2 * params.D.y;
-    if (y1 < cyl->center.y || y1 > cyl->center.y + cyl->height)
-        t1 = INFINITY;
-    if (y2 < cyl->center.y || y2 > cyl->center.y + cyl->height)
-        {t2 = INFINITY;}
+    // double y1 = params.O.y + t1 * params.D.y;
+    // double y2 = params.O.y + t2 * params.D.y;
+    // if (y1 < cyl->center.y || y1 > cyl->center.y + cyl->height)
+    //     t1 = INFINITY;
+    // if (y2 < cyl->center.y || y2 > cyl->center.y + cyl->height)
+    //     {t2 = INFINITY;}
+    // For t1
+    vector P1 = vector_add(params.O, vector_scale(params.D, t1));
+    double axis_dist1 = vector_dot(vector_sub(P1, cyl->center), cyl->axis);
+    if (axis_dist1 < -cyl->height / 2 || axis_dist1 > cyl->height / 2)
+        t1 = INFINITY; // Intersection is outside the caps
+
+    // For t2
+    vector P2 = vector_add(params.O, vector_scale(params.D, t2));
+    double axis_dist2 = vector_dot(vector_sub(P2, cyl->center), cyl->axis);
+    if (axis_dist2 < -cyl->height / 2 || axis_dist2 > cyl->height / 2)
+        {t2 = INFINITY; }// Intersection is outside the caps
 	if (update_result(result, t1, cyl, params))//two if statements are necessary
     {
         result->type = SHAPE_CYLINDER;
@@ -93,15 +117,20 @@ int intersect_ray_cylinder(ray_params params, cylinder *cyl, intersection_result
     }
     // Intersect with the bottom cap
     plane bottom_cap;
-    bottom_cap.point = cyl->center; // Center of the bottom cap
-    bottom_cap.normal = vector_init(0, -1, 0); // Normal pointing downwards
+    // bottom_cap.point = cyl->center; // Center of the bottom cap
+    // bottom_cap.normal = vector_init(0, -1, 0); // Normal pointing downwards
+    bottom_cap.point = bottom_center;
+    bottom_cap.normal = vector_scale(cyl->axis, -1); // Normal pointing outward from the cap
+
 
     double t_cap;
     if (intersect_ray_plane(params, &bottom_cap, &t_cap))
     {
         vector P = vector_add(params.O, vector_scale(params.D, t_cap));
-        vector dist_vec = vector_sub(P, cyl->center);
-        double dist_sq = dist_vec.x * dist_vec.x + dist_vec.z * dist_vec.z;
+        //vector dist_vec = vector_sub(P, cyl->center);
+        vector dist_vec = vector_sub(P, bottom_cap.point);
+        vector dist_vec_perp = vector_sub(dist_vec, vector_scale(cyl->axis, vector_dot(dist_vec, cyl->axis)));
+        double dist_sq = vector_dot(dist_vec_perp, dist_vec_perp);
         if (dist_sq <= cyl->radius * cyl->radius)
         {
             if (update_result(result, t_cap, cyl, params))
@@ -114,14 +143,18 @@ int intersect_ray_cylinder(ray_params params, cylinder *cyl, intersection_result
 
     // Intersect with the top cap
     plane top_cap;
-    top_cap.point = vector_add(cyl->center, vector_init(0, cyl->height, 0)); // Center of the top cap
-    top_cap.normal = vector_init(0, 1, 0); // Normal pointing upwards
+    // top_cap.point = vector_add(cyl->center, vector_init(0, cyl->height, 0)); // Center of the top cap
+    // top_cap.normal = vector_init(0, 1, 0); // Normal pointing upwards
+    top_cap.point = top_center;
+    top_cap.normal = cyl->axis; // Normal pointing outward from the cap
 
     if (intersect_ray_plane(params, &top_cap, &t_cap))
     {
         vector P = vector_add(params.O, vector_scale(params.D, t_cap));
         vector dist_vec = vector_sub(P, top_cap.point);
-        double dist_sq = dist_vec.x * dist_vec.x + dist_vec.z * dist_vec.z;
+        vector dist_vec_perp = vector_sub(dist_vec, vector_scale(cyl->axis, vector_dot(dist_vec, cyl->axis)));
+        //double dist_sq = dist_vec.x * dist_vec.x + dist_vec.z * dist_vec.z;
+        double dist_sq = vector_dot(dist_vec_perp, dist_vec_perp);
         if (dist_sq <= cyl->radius * cyl->radius)
         {
             if (update_result(result, t_cap, cyl, params))
--- render.c
+++ render.c
@@ -52,7 +52,7 @@ scene create_scene()
     scene.spheres = malloc(sizeof(sphere) * scene.sphere_count);
 
     // Red sphere
-    scene.spheres[0] = (sphere){{0, -1, 3}, 1, {255, 255, 0, 500, 0.4}, 1};
+    scene.spheres[0] = (sphere){{0, -1, 3}, 1, {255, 0, 0, 500, 0.4}, 1};
     // Blue sphere
    // scene.spheres[1] = (sphere){{-2, 0, 4}, 1, 0, 0, 255, 10, 0.3};
     // Green sphere
@@ -63,13 +63,13 @@ scene create_scene()
     // Example plane
     scene.plane_count = 1;
     scene.planes = malloc(sizeof(plane) * scene.plane_count);
-    scene.planes[0] = (plane){{0, -2, 0}, {0, 1, 0}, {255, 255, 0, 30, 0.5}, 2};
+    scene.planes[0] = (plane){{0, -2, 0}, {0, 1, 0}, {255, 255, 255, 30, 0.5}, 2};
 
     scene.cylinder_count = 1;
     scene.cylinders = malloc(sizeof(cylinder) * scene.cylinder_count);
 
     // Example cylinder
-    scene.cylinders[0] = (cylinder){{-1, -3, 4}, 1.5, 2, {0, 1, 0}, {0, 0, 255, 500, 0.3}, 1};
+    scene.cylinders[0] = (cylinder){{-1, -1, 4}, 1.5, 2, {0, 1, 0}, {0, 0, 255, 500, 0.3}, 1};
 
 
    scene.light_count = 3;
