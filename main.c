33dbe0864f78d060f04b72f63879a87939c309ae
Michaela M <masarova.michaela@gmail.com>
2024-10-02 18:32:36 +0300

    Feat: filename management

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include "minirt.h"
 
+int	get_fd(char *filename, t_canvas *canvas)
+{
+	int	fd;
+
+	if (ft_strcmp(filename + ft_strlen(filename) - 3, ".rt"))
+		return (printf("Error: Invalid file extension\n"), -1);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (perror("Error:"), free_close(canvas, fd), -1);
+	return (fd);
+}
+
 int	main(int argc, char **argv)
 {
 	t_canvas	*canvas;
@@ -13,9 +25,9 @@ int	main(int argc, char **argv)
 		if (canvas == NULL)
 			return (1);
 		//scene scene = create_scene(); // change to parcing from .rt file
-		fd = open(argv[1], O_RDONLY);
+		fd = get_fd(argv[1], canvas);
 		if (fd == -1)
-			return (perror("Error:"), free_close(canvas, fd), 1);
+			return (1);
 		scene = parse_rt(fd, argv[1]);
 		if (scene.success)
 			return (free_everything(scene, canvas, fd), 1);
